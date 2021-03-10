a8cb496fd1647ad8b741a85fe7f2384a2d7aa6c8
Abdeljalil El hachimi <ael-hach@e1r6p12.1337.ma>
2021-03-10 18:30:39 +0100

    fixes some bugs

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -28,12 +28,12 @@ func TestNextToken(t *testing.T) {
 	for i, tt := range tests {
 		tok := l.NextToken()
 		if tok.Type != tt.expectedType {
-			t.Fatal("tests[%d] - tokentype wrong. expected=%q, got=%q",
+			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q",
 					i, tt.expectedType, tok.Type)
 		}
 
-		if tok.Literal != tt.expectedType {
-			t.Fatal("tests[%d] - literal wrong. expected=%q, got=%q",
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q",
 					i,  tt.expectedLiteral, tok.Literal)
 		}
 	}
